2ddf406ebe11cf9b115f76e1d883c8842fbfde85
SgtKOCD <44818302+SgtKOCD@users.noreply.github.com>
2021-11-25 16:56:43 +0300

    Add files via upload

---
+++ ÖDEV3.sql
@@ -0,0 +1,4 @@
+select country from country where country like 'A%a';
+select country from country where country like '______%';
+select title from film where title ilike '%t%t%t%t%';
+select title from film where title like 'C%' and length > 90 and rental_rate = 2.99;
